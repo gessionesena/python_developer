a8b025f2e4b100937a81fe2199fed41d53c63d3f
Gessione Sena <fgssena@gmail.com>
2023-11-17 16:03:40 -0300

    atualizações de funções do sistema

--- sistema_bancario_v2.py
+++ sistema_bancario_v2.py
@@ -1,8 +1,8 @@
 menu = '''
-[d] Depósito
-[s] Saque
-[e] Extrato
-[q] Sair
+[D] Depósito       [CU] Cadastrar Usuário
+[S] Saque          [BU] Buscar Usuário
+[E] Extrato        [CC] Cadastrar Conta Corrente
+[Q] Sair           [BC] Buscar Contas
 
 '''
 saldo = 0
@@ -10,7 +10,8 @@ qtd_saques = 0
 limite = 500
 LIMITE_SAQUES = 3
 extrato = ''
-
+usuarios = [{"cpf":'02518026398',"nome":'Gessione Sena', "data_nasc":'14/06/1986',"endereco":'Cajueiro do Boi'}]
+contas = []
 #**********Função para depósito**********************************
 def depositar(valor_deposito,saldo_total,/):#argumentos chamados apenas por posição
     saldo_atual = saldo_total + valor_deposito
@@ -24,7 +25,7 @@ def sacar(*,valor_saque,saldo_total):#argumentos chamados apenas por nomes
     return saldo_atual    
 #****************************************************************
 #**********Função para extrato**********************************
-def mostrar_extrato(movimentacao,/,*,saldo_total):#argumentos chamados apenas por posição
+def mostrar_extrato(movimentacao,/,*,saldo_total):#argumentos chamados por nome e posição
     if extrato == '':
         #saida = (f"\n{f'Sem movimentação':>31}" if extrato == '' else movimentacao)
         saida = f"{f'Sem movimentação':>31}\n{'_'*31}\n{f'Saldo: R$ {saldo_total:.2f}  ':>31}\n"
@@ -33,24 +34,95 @@ def mostrar_extrato(movimentacao,/,*,saldo_total):#argumentos chamados apenas po
     
     return saida
 #****************************************************************
-
+#**********Função criar usuario**********************************
+def criar_usuario(usuarios,cpf): 
+    teste = False
+    for usuario in usuarios:
+        item = usuario['cpf']
+        if item == cpf: 
+            teste = True
+    if teste == False:    
+        nome = input('Nome completo: ')
+        data_nasc = input('Data de Nascimento: ')
+        endereco = input('Endereço completo: ')
+        dados_usuario = dict({"cpf":cpf,"nome":nome, "data_nasc":data_nasc,"endereco":endereco})
+        
+    return dados_usuario if teste == False else f'O CPF {cpf} já existe na base de dados!'
+#****************************************************************
+#**********Função buscar usuario**********************************
+def buscar_usuario(usuarios,cpf): 
+    teste = False
+    for usuario in usuarios:
+        item = usuario['cpf']
+        if item == cpf: 
+            teste = True
+            resultado = usuario 
+           
+    return resultado if teste == True else f'O CPF {cpf} não existe na base de dados!'
+#****************************************************************
+#**********Função criar conta**********************************
+def abrir_conta(usuarios,contas,cpf):
+    teste = False
+    for usuario in usuarios:
+        item = usuario['cpf']
+        if item == cpf:
+            teste = True
+    if teste == True:
+        AGENCIA = '0001'
+        conta_corrente = len(contas) + 1
+        dados_conta = dict({'conta':conta_corrente,'agencia':AGENCIA, 'usuario':cpf})  
+    return dados_conta if teste == True else f'Usuário {cpf} não existe no banco de dados!'
+#****************************************************************
+#**********Função listar contas**********************************
+def buscar_contas(contas,cpf):
+    list = []
+    for conta in contas:
+        item = conta['usuario']
+        if cpf == item:
+            list.append(conta)
+    return f'Não há contas vinculadas ao CPF informado!' if list == [] else list
+    
+#****************************************************************
 while True:
-    opcao = input(menu)
+    opcao = input(menu).upper()
     
     #MÓDULO DEPÓSITO
-    if opcao == 'd' or opcao == 'D':
-
-        valor = float(input('Digite o valor a ser depositado: '))
-        if valor > 0:
-            saldo = depositar(valor,saldo)#atualizando a variavel saldo com o retorno da função depositar 
-            extrato = f"{extrato}\n {f'R$ {valor:.2f} C':>30}"
-            print(f"Depósito no valor de R$ {valor:.2f} realizado com sucesso!")
-            
+    if opcao == 'D':
+        cpf_usuario = input("Digite o CPF do titular da conta: ").strip()   
+        busca = buscar_usuario(usuarios,cpf_usuario)#buscando usuario no banco de dados
+        
+        if isinstance(busca,dict):#caso o usuario exista, é hora de verificar se possui conta aberta
+            search = buscar_contas(contas,cpf_usuario)
+            if isinstance(search,list):
+                print(f"Contas vinculadas ao CPF {cpf_usuario}:\n{'_'*20}")
+                for item in search:#listando todas as contas vinculadas ao usuario
+                    print(f"Agência: {item['agencia']}\nConta Nº: {item['conta']}\n{'-'*20}")
+                if len(search) == 1:#se usuario possuir apenas 1 conta
+                    valor = float(input('Digite o valor a ser depositado: '))
+                    if valor > 0:
+                        saldo = depositar(valor,saldo)#atualizando a variavel saldo com o retorno da função depositar 
+                        extrato = f"{extrato}\n {f'R$ {valor:.2f} C':>30}"
+                        print(f"Depósito no valor de R$ {valor:.2f} realizado com sucesso!")
+                        
+                    else:
+                        print('Valor incorreto. Tente outro valor!')
+                else:#caso possua mais de uma conta, deverá escolher
+                    escolha_conta = input('Qual conta deseja depositar? Digite o N° da conta: ')
+                    valor = float(input('Digite o valor a ser depositado: '))
+                    if valor > 0:
+                        saldo = depositar(valor,saldo)#atualizando a variavel saldo com o retorno da função depositar 
+                        extrato = f"{extrato}\n {f'R$ {valor:.2f} C':>30}"
+                        print(f"Depósito no valor de R$ {valor:.2f} realizado com sucesso!")
+                        
+                    else:
+                        print('Valor incorreto. Tente outro valor!')
+            else:
+                print(search)
         else:
-            print('Valor incorreto. Tente outro valor!')
+            print(busca)
     
     #MÓDULO SAQUE
-    elif opcao == 's' or opcao == 'S':
+    elif opcao == 'S':
         valor = float(input('Digite o valor a ser sacado:'))
         lim_valor_excedido = valor > limite
         lim_saque_excedido = qtd_saques >= LIMITE_SAQUES
@@ -71,14 +143,57 @@ while True:
                 print('Saldo Insuficiente!')
 
     #MÓDULO EXTRATO
-    elif opcao == 'e' or opcao == 'E':
+    elif opcao == 'E':
         
         print(f"{'*'*31}\n{'E X T R A T O':^31}\n{'_'*31}")
         print(mostrar_extrato(extrato,saldo_total=saldo))#chamando função mostrarextrato() com argumentos posicionais e nominais
         print(f"{'*' *31}")  
+    
+    elif opcao == 'C':
+        cpf_usuario = input('Digite o CPF do usuário: ').strip()
+
+        novo_usuario = criar_usuario(usuarios,cpf_usuario)
+        usuarios.append(novo_usuario) if isinstance(novo_usuario,dict) else print(novo_usuario)#caso a saida da função seja um dicionario, adiciona a lista usuarios, caso contrario, imprime uma mensagem
+        if isinstance(novo_usuario,dict):#se a variavel novo_usuario é um dicionario
+            print(f"Usuário {novo_usuario['nome']} foi cadastrado com sucesso!")
+        
+        print(usuarios)
+    
+    elif opcao == 'B':
+        cpf_usuario = input("Informe o CPF: ").strip()
+        search = buscar_usuario(usuarios,cpf_usuario)
+        print(search)
+
+    elif opcao == 'CC':
+        cpf_usuario = input("Iremos abrir sua conta corrente. Informe o CPF: ").strip()
+        
+        nova_conta = abrir_conta(usuarios,contas,cpf_usuario)
+        contas.append(nova_conta) if isinstance(nova_conta,dict) else print(nova_conta)
+
+        if isinstance(nova_conta, dict):#se a variavel nova_conta é um dicionario
+            print(f"Conta Corrente Nº {nova_conta['conta']}, Ag: {nova_conta['agencia']} cadastrada com sucesso! ")
+        
+    elif opcao == 'BC':
+
+        while True:#laço para validar o cpf
+            cpf_usuario = input("Buscar contas vinculadas ao CPF: ").strip()
+            if len(cpf_usuario) == 11:
+                break
+            else:
+                print('Digite um CPF válido:' )
+        
+        contas_por_cpf = buscar_contas(contas,cpf_usuario)#adiciona à variavel todas as contas vinculadas ao cpf caso possua
+        if isinstance(contas_por_cpf,list):#se a variavel contas_por_cpf for uma lista
+            print(f"Contas vinculadas ao CPF {cpf_usuario}:\n{'_'*20}")
+            for item in contas_por_cpf:
+                print(f"Agência: {item['agencia']}\nConta Nº: {item['conta']}\n{'-'*20}")
+        else:
+            print(contas_por_cpf)#imprime uma mensagem
+
+        
 
     #SAIR
-    elif opcao == 'q' or opcao == 'Q':
+    elif opcao == 'Q':
         break
     
     else:
